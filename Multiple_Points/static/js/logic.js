ba83367a6b4b145401f79175227c8aff1a1674ca
Jack Key <jhschulte44@gmail.com>
2021-11-28 21:04:30 -0500

    Adding code

--- Multiple_Points/static/js/logic.js
+++ Multiple_Points/static/js/logic.js
@@ -1,5 +1,5 @@
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 6);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // An array containing each city's location, state, and population.
 let cities = [{
@@ -37,7 +37,12 @@ let cities = [{
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location, {
+      radius: city.population/200000,
+      color: "orange"
+    }).addTo(map)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
    });
 
 // We create the tile layer that will be the background of our map.
